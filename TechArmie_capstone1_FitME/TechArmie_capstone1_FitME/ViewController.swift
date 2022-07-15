8457dcca8e3186cb2d4b3ad1938c7fcd1bccebe0
Aanchal <bansalaanchal96@gmail.com>
2022-07-15 19:14:17 -0400

    Project Setup

---
+++ TechArmie_capstone1_FitME/TechArmie_capstone1_FitME/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechArmie_capstone1_FitME
+//
+//  Created by Aanchal Bansal on 2022-07-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
