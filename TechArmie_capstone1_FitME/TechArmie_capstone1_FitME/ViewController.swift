9d0834e47232f97ef479030da4b5bb680029d6af
BhautikPethani <37444804+pethani42@users.noreply.github.com>
2022-07-20 18:42:44 -0400

    CAP - 2 and CAP - 3
    Video Integration and UI Implementation, after Splash Screen Branch deleted

--- TechArmie_capstone1_FitME/TechArmie_capstone1_FitME/ViewController.swift
+++ TechArmie_capstone1_FitME/TechArmie_capstone1_FitME/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var videoLayer: UIView!
+    @IBOutlet weak var welcomeImg: UIImageView!
+    @IBOutlet weak var lblAlreadyHaveAccount: UILabel!
+    @IBOutlet weak var btnNewAccount: UIButton!
+    @IBOutlet weak var btnLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        playVideo()
     }
 
+    func playVideo(){
+        guard let path = Bundle.main.path(forResource: "bg_video", ofType: "mp4") else{
+            return
+        }
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        self.videoLayer.layer.addSublayer(playerLayer)
+        
+        player.play()
+        
+        videoLayer.bringSubviewToFront(welcomeImg)
+        videoLayer.bringSubviewToFront(btnNewAccount)
+        videoLayer.bringSubviewToFront(btnLogin)
+        videoLayer.bringSubviewToFront(lblAlreadyHaveAccount)
+    }
 
 }
 
