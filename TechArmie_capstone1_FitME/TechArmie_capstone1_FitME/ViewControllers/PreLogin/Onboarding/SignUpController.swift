e68580ca0f5129893b4a3b5811d846c03633e14a
Swapnil Kumbhar <swapkumbhar31@gmail.com>
2022-07-21 19:15:45 -0400

    CAP-38 Sign UP Page UI completed

---
+++ TechArmie_capstone1_FitME/TechArmie_capstone1_FitME/ViewControllers/PreLogin/Onboarding/SignUpController.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpController.swift
+//  TechArmie_capstone1_FitME
+//
+//  Created by Swapnil Kumbhar on 2022-07-21.
+//
+
+import Foundation
+import UIKit
+
+
+class SignUpController: UIViewController {
+    
+}
