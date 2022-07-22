0247be126146ae29f2e04e33ef03be491dce91ef
VirMacpro1996 <virdavinder1996@gmail.com>
2022-07-22 03:09:01 -0400

    CAP-31 SignUp Step-2

---
+++ TechArmie_capstone1_FitME/TechArmie_capstone1_FitME/ViewControllers/PreLogin/Onboarding/SecondStepVC.swift
@@ -0,0 +1,23 @@
+//
+//  SecondStepVC.swift
+//  TechArmie_capstone1_FitME
+//
+//  Created by Vir Davinder Singh on 2022-07-22.
+//
+
+
+import Foundation
+import UIKit
+class SecondStepVC : UIViewController
+{
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
