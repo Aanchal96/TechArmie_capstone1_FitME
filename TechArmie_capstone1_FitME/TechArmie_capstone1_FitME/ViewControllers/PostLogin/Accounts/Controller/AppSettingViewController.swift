e2a55cd02770b2dec47aac66ce2a4f76c23c3ca2
BhautikPethani <c0854487@mylambton.ca>
2022-08-04 18:30:10 -0400

    Terms and conditions web view UI part completed

--- TechArmie_capstone1_FitME/TechArmie_capstone1_FitME/ViewControllers/PostLogin/Accounts/Controller/AppSettingViewController.swift
+++ TechArmie_capstone1_FitME/TechArmie_capstone1_FitME/ViewControllers/PostLogin/Accounts/Controller/AppSettingViewController.swift
@@ -13,11 +13,14 @@ class AppSettingViewController: UIViewController {
     @IBOutlet weak var accountSettingView: UIView!
     @IBOutlet weak var shareView: UIView!
     @IBOutlet weak var rateUsView: UIView!
+    @IBOutlet weak var termsAndConditionsView: UIView!
+    @IBOutlet weak var privacyPolicyView: UIView!
     
     @IBOutlet weak var profilePicture: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationController?.navigationBar.isHidden = true
         addTapsOnView()
 //        let childView = UIHostingController(rootView: AppSettingView(controller: self));
 //        addChild(childView);
@@ -78,12 +81,18 @@ class AppSettingViewController: UIViewController {
         if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
             SKStoreReviewController.requestReview(in: scene)
         }
+    @objc func termsAndConditionTap(tapGestureRecognizer: UITapGestureRecognizer) {
+        let vc = TermsAndConditionsViewController.instantiate(fromAppStoryboard: .Account)
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func privacyPolicyTap(tapGestureRecognizer: UITapGestureRecognizer) {
+        
     }
 }
 extension AppSettingViewController{
     func addTapsOnView(){
         let accountSettingTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(accountSettingTap(tapGestureRecognizer:)))
-        
         accountSettingView.isUserInteractionEnabled = true
         accountSettingView.addGestureRecognizer(accountSettingTapGestureRecognizer)
         
@@ -94,5 +103,8 @@ extension AppSettingViewController{
         let rateUsTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(rateUs(tapGestureRecognizer:)))
         rateUsView.isUserInteractionEnabled = true;
         rateUsView.addGestureRecognizer(rateUsTapGestureRecognizer)
+        let termsAndConditionTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(termsAndConditionTap(tapGestureRecognizer:)))
+        termsAndConditionsView.isUserInteractionEnabled = true
+        termsAndConditionsView.addGestureRecognizer(termsAndConditionTapGestureRecognizer)
     }
 }
