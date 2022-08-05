489ce953658ee9cc817fc5f92a20d8b233950862
Swapnil Kumbhar <swapkumbhar31@gmail.com>
2022-08-04 23:35:18 -0400

    CAP-71 google service info list updated

--- TechArmie_capstone1_FitME/TechArmie_capstone1_FitME/ViewControllers/PostLogin/Accounts/Controller/AppSettingViewController.swift
+++ TechArmie_capstone1_FitME/TechArmie_capstone1_FitME/ViewControllers/PostLogin/Accounts/Controller/AppSettingViewController.swift
@@ -19,14 +19,8 @@ class AppSettingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.navigationController?.navigationBar.isHidden = true
         addTapsOnView()
-//        let childView = UIHostingController(rootView: AppSettingView(controller: self));
-//        addChild(childView);
-//        childView.view.frame = view.bounds;
-//        view.addSubview(childView.view);
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func logout(_ sender: Any) {
